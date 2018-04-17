a9fc74ec67ae0a35840c76e094aa0d22d21e7ddd
Maja Jelenik <maja.jelenik@vp.pl>
2018-04-17 17:26:58 +0200

    Change js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,10 @@
-var a = prompt("Podaj a");
-var h = prompt("Podaj h");
-var triangleArea = getTriangleArea (a, h),
-	triangle1Area = getTriangleArea(1, 1),
-	triangle2Area = getTriangleArea(5, 5),
-	triangle3Area = getTriangleArea(10, 10);
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = maleNames.concat(femaleNames);
+var newName = 'Marian';
 
-
-function getTriangleArea (a, h) {
-
-	if ((a <= 0) && (h <= 0)) {
-		document.write("Nieprawidłowe dane");
-	} else if ((a > 0) || (h > 0)) {
-		return a * h / 2;
-	}
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
 }
 
-console.log(getTriangleArea(10, 6));
-
-
-
+console.log(allNames);
